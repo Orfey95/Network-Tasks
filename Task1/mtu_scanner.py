fa3134c22a673ffdf3f1a89aa5a0cfc9652c3b9f
Sasha <sasha7692@gmail.com>
2020-03-04 23:27:39 +0200

    Update mtu_scanner.py

--- Task1/mtu_scanner.py
+++ Task1/mtu_scanner.py
@@ -4,7 +4,7 @@ import sys
 
 def mtu_scanner():
     hostname = sys.argv[1]
-    size = 1
+    size = 0
     while True:
         response = os.system("ping -M do -c 1 -i 0.2 -s " + str(size) + " " + hostname)
         size += 50
