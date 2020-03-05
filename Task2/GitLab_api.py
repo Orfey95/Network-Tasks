b3902af9a2daf2a131dacddf94b6a78071e3617b
Sasha <sasha7692@gmail.com>
2020-03-05 23:43:42 +0200

    Update GitLab_api.py

--- Task2/GitLab_api.py
+++ Task2/GitLab_api.py
@@ -37,6 +37,17 @@ def get_group_id_by_name(group_name, private_token):
         return -1
 
 
+def get_milestone_id_by_name(milestone_name, user_name, project_name, private_token):
+    user_id = get_user_id_by_name(user_name, private_token)
+    project_id = get_project_id_by_name(project_name, user_id, private_token)
+    response = requests.get(f'https://gitlab.com/api/v4/projects/{project_id}/milestones?title={milestone_name}&private_token={private_token}')
+    milestone_id_list = re.findall(r'"id":(.*?),', str(response.content))
+    if len(milestone_id_list) != 0:
+        return milestone_id_list[0]
+    else:
+        return -1
+
+
 def create_project(name, group_name, private_token):
     namespace_id = get_group_id_by_name(group_name, private_token)
     if namespace_id != -1:
@@ -94,18 +105,24 @@ def change_tag(act, project_name, user_name, tag_name, ref, private_token):
         sys.exit(__ERROR_ARGUMENT)
 
 
-def create_issue(project_id, title, due_date, labels, milestone_id, private_token):
-    check_milestone_id = requests.get(f'https://gitlab.com/api/v4/projects/{project_id}/milestones/{milestone_id}?private_token={private_token}')
-    if check_milestone_id.status_code != 404:
-        response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/issues?title={title}&due_date={due_date}&labels={labels}&milestone_id={milestone_id}&private_token={private_token}')
+def create_issue(project_name, user_name, title, due_date, labels, milestone_name, private_token):
+    user_id = get_user_id_by_name(user_name, private_token)
+    project_id = get_project_id_by_name(project_name, user_id, private_token)
+    milestone_id = get_milestone_id_by_name(milestone_name, user_name, project_name, private_token)
+    if milestone_id != -1:
+        response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/issues?'
+                                 f'title={title}&'
+                                 f'due_date={due_date}&'
+                                 f'labels={labels}&'
+                                 f'milestone_id={milestone_id}&'
+                                 f'private_token={private_token}')
+        return response
     else:
-        response = 'No such id'
-    # milestone_id = (re.findall(r'"milestone":(.*?),', str(response.content)))[0]
-    # print(milestone_id)
-    return response
+        # create milestone
+        pass
 
 
 # print(create_project('new_147', 'jhjh', '2hQuku5zYXvrgniuFMHL'))
 # print(change_member('add', 'new_pasdgja', 'oleksandr_frolov', 'jkbroker', 30, '2hQuku5zYXvrgniuFMHL'))
 # print(change_tag('add', 'new_pasdgja', 'oleksandr_frolov', 'new_tag', 'master', '2hQuku5zYXvrgniuFMHL'))
-# print(create_issue(17306214, 'try', '2020-03-11', 'new_label, new_label2', 1181925, '2hQuku5zYXvrgniuFMHL'))
+# print(create_issue(new_pasdgja, 'oleksandr_frolov', 'try', '2020-03-11', 'new_label, new_label2', 1181925, '2hQuku5zYXvrgniuFMHL'))
