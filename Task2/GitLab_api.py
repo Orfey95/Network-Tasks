cf09f12fd916595db70d2b2752ece28e2a38f368
Sasha <sasha7692@gmail.com>
2020-03-05 23:20:45 +0200

    Update GitLab_api.py

--- Task2/GitLab_api.py
+++ Task2/GitLab_api.py
@@ -1,50 +1,103 @@
+import sys
 import requests
 import re
 
 
-def create_project(project_name, group, token):
-    response = requests.post(f'https://gitlab.com/api/v4/projects?'
-                             f'name={project_name}'
-                             f'&namespace_id={group}'
-                             f'&private_token={token}')
-    return response
+__ERROR_ARGUMENT = 2  # problem with arguments
 
 
-def change_member(act, project_id, user_id, access_level, token):
-    if act == 'add':
-        response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/members?'
-                                 f'user_id={user_id}&'
-                                 f'access_level={access_level}&'
-                                 f'private_token={token}')
+def get_user_id_by_name(user_name, private_token):
+    response = requests.get(f'https://gitlab.com/api/v4/users/?username={user_name}&private_token={private_token}')
+    user_id_list = re.findall(r'"id":(.*?),', str(response.content))
+    if len(user_id_list) != 0:
+        return user_id_list[0]
+    else:
+        return -1
+
+
+def get_project_id_by_name(project_name, user_id, private_token):
+    response = requests.get(f'https://gitlab.com/api/v4/users/{user_id}/projects?private_token={private_token}')
+    project_name_list = re.findall(r'"name":"(.*?)",', str(response.content))
+    project_id_list = re.findall(r'"id":(.*?),', str(response.content))
+    try:
+        temp = project_name_list.index(project_name)
+        return project_id_list[temp]
+    except:
+        return -1
+
+
+def get_group_id_by_name(group_name, private_token):
+    response = requests.get(f'https://gitlab.com/api/v4/groups?statistics=true&private_token={private_token}')
+    group_name_list = re.findall(r'"name":"(.*?)",', str(response.content))
+    group_id_list = re.findall(r'"id":(.*?),', str(response.content))
+    try:
+        temp = group_name_list.index(group_name)
+        return group_id_list[temp]
+    except:
+        return -1
+
+
+def create_project(name, group_name, private_token):
+    namespace_id = get_group_id_by_name(group_name, private_token)
+    if namespace_id != -1:
+        response = requests.post(f'https://gitlab.com/api/v4/projects?'
+                                 f'name={name}'
+                                 f'&namespace_id={namespace_id}'
+                                 f'&private_token={private_token}')
         return response
-    if act == 'del':
-        response = requests.delete(f'https://gitlab.com/api/v4/projects/{project_id}/members/{user_id}?'
-                                   f'private_token={token}')
-        return response
-    if act == 'change':
-        response = requests.put(f'https://gitlab.com/api/v4/projects/{project_id}/members/{user_id}?'
-                                f'access_level={access_level}&'
-                                f'private_token={token}')
-        return response
+    else:
+        print('No group with this name exists.')
+        sys.exit(__ERROR_ARGUMENT)
 
 
-def change_tag(act, project_id, tag_name, ref, token):
-    if act == 'add':
-        response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/repository/tags?'
-                                 f'tag_name={tag_name}&'
-                                 f'ref={ref}&'
-                                 f'private_token={token}')
-        return response
-    if act == 'del':
-        response = requests.delete(f'https://gitlab.com/api/v4/projects/{project_id}/repository/tags/{tag_name}?'
-                                   f'private_token={token}')
-        return response
+def change_member(act, project_name, user_name, member_name, access_level, private_token):
+    member_id = get_user_id_by_name(member_name, private_token)
+    user_id = get_user_id_by_name(user_name, private_token)
+    project_id = get_project_id_by_name(project_name, user_id, private_token)
+    if user_id and project_id != -1:
+        if act == 'add':
+            response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/members?'
+                                     f'user_id={member_id}&'
+                                     f'access_level={access_level}&'
+                                     f'private_token={private_token}')
+            return response
+        if act == 'del':
+            response = requests.delete(f'https://gitlab.com/api/v4/projects/{project_id}/members/{member_id}?'
+                                       f'private_token={private_token}')
+            return response
+        if act == 'change':
+            response = requests.put(f'https://gitlab.com/api/v4/projects/{project_id}/members/{member_id}?'
+                                    f'access_level={access_level}&'
+                                    f'private_token={private_token}')
+            return response
+    else:
+        print('No user or project with this name exists.')
+        sys.exit(__ERROR_ARGUMENT)
 
 
-def create_issue(project_id, title, due_date, labels, milestone_id, token):
-    check_milestone_id = requests.get(f'https://gitlab.com/api/v4/projects/{project_id}/milestones/{milestone_id}?private_token={token}')
+def change_tag(act, project_name, user_name, tag_name, ref, private_token):
+    user_id = get_user_id_by_name(user_name, private_token)
+    project_id = get_project_id_by_name(project_name, user_id, private_token)
+    if user_id and project_id != -1:
+        if act == 'add':
+            response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/repository/tags?'
+                                     f'tag_name={tag_name}&'
+                                     f'ref={ref}&'
+                                     f'private_token={private_token}')
+            return response
+        if act == 'del':
+            response = requests.delete(f'https://gitlab.com/api/v4/projects/{project_id}/repository/tags/{tag_name}?'
+                                       f'private_token={private_token}')
+            return response
+    else:
+        print('No user or project with this name exists.')
+        sys.exit(__ERROR_ARGUMENT)
+
+
+def create_issue(project_id, title, due_date, labels, milestone_id, private_token):
+    check_milestone_id = requests.get(f'https://gitlab.com/api/v4/projects/{project_id}/milestones/{milestone_id}?private_token={private_token}')
     if check_milestone_id.status_code != 404:
-        response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/issues?title={title}&due_date={due_date}&labels={labels}&milestone_id={milestone_id}&private_token={token}')
+        response = requests.post(f'https://gitlab.com/api/v4/projects/{project_id}/issues?title={title}&due_date={due_date}&labels={labels}&milestone_id={milestone_id}&private_token={private_token}')
     else:
         response = 'No such id'
     # milestone_id = (re.findall(r'"milestone":(.*?),', str(response.content)))[0]
@@ -52,7 +105,7 @@ def create_issue(project_id, title, due_date, labels, milestone_id, token):
     return response
 
 
-print(create_project('new_147', 7337546, '2hQuku5zYXvrgniuFMHL'))
-# print(change_member('change', 17306214, 3496872, 10, '2hQuku5zYXvrgniuFMHL'))
-# print(change_tag('add', 17306214, 'new_tag', 'master', '2hQuku5zYXvrgniuFMHL'))
+# print(create_project('new_147', 'jhjh', '2hQuku5zYXvrgniuFMHL'))
+# print(change_member('add', 'new_pasdgja', 'oleksandr_frolov', 'jkbroker', 30, '2hQuku5zYXvrgniuFMHL'))
+# print(change_tag('add', 'new_pasdgja', 'oleksandr_frolov', 'new_tag', 'master', '2hQuku5zYXvrgniuFMHL'))
 # print(create_issue(17306214, 'try', '2020-03-11', 'new_label, new_label2', 1181925, '2hQuku5zYXvrgniuFMHL'))
