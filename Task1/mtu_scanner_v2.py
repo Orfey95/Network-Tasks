dc20de029d397706fa5bd9bd9524952a43e54b93
Sasha <sasha7692@gmail.com>
2020-03-05 10:30:08 +0200

    Update mtu_scanner_v2.py

--- Task1/mtu_scanner_v2.py
+++ Task1/mtu_scanner_v2.py
@@ -6,7 +6,7 @@ def mtu_scanner():
     hostname = sys.argv[1]
     size = 1400
     while True:
-        response = os.system("ping -M do -c 1 -i 0.2 -s " + str(size) + " " + hostname)
+        response = os.system("ping -M do -c 1 -i 0.2 -s " + str(size) + " " + hostname + " > /dev/null")
         size += 1
         if response == 256:
             return f"MTU is {size + 26}"
