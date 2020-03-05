5789686a319602db8b5a8651b3c264be518fe042
Sasha <sasha7692@gmail.com>
2020-03-05 10:26:32 +0200

    Create mtu_scanner_v2.py

---
+++ Task1/mtu_scanner_v2.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+def mtu_scanner():
+    hostname = sys.argv[1]
+    size = 1400
+    while True:
+        response = os.system("ping -M do -c 1 -i 0.2 -s " + str(size) + " " + hostname)
+        size += 1
+        if response == 256:
+            return f"MTU is {size + 26}"
+
+
+if __name__ == "__main__":
+    print(mtu_scanner())
