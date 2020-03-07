a6ae9dcfe05b02483c5eedf0d5571a5098cde883
Sasha <sasha7692@gmail.com>
2020-03-07 19:06:21 +0200

    Update DHCP-1.sh

--- Task3/provisions/DHCP-1.sh
+++ Task3/provisions/DHCP-1.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Hello world!"
+
+
+# Install DHCP server
+apt install -y isc-dhcp-server
+# Configuration
+rm /etc/dhcp/dhcpd.conf
+cp /vagrant/DHCP-1/dhcpd.conf /etc/dhcp
+# Interfaces
+rm /etc/default/isc-dhcp-server
+cp /vagrant/DHCP-1/isc-dhcp-server /etc/default
+# Restart DHCP
+service isc-dhcp-server restart
+# Check status DHCP
+systemctl status isc-dhcp-server
