a9939a97ce86d35f0aee1329b5d45bec16f9ccab
Sasha <sasha7692@gmail.com>
2020-03-07 19:06:46 +0200

    Update DHCP-2.sh

--- Task3/provisions/DHCP-2.sh
+++ Task3/provisions/DHCP-2.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo "Hello world!"
+
+
+# Install DHCP server
+yum --quiet install -y dhcp
+# Configuration
+rm /etc/dhcp/dhcpd.conf
+cp /vagrant/DHCP-2/dhcpd.conf /etc/dhcp
+# Enable DHCP
+systemctl enable dhcpd
+# Start DHCP
+systemctl start dhcpd
+# Interfaces
+rm /etc/sysconfig/dhcpd
+cp /vagrant/DHCP-2/dhcpd /etc/sysconfig
+# Restart DHCP
+systemctl restart dhcpd
+# Check status DHCP
+systemctl status dhcpd
