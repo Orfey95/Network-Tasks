6e29edb4b3ad08d13c3ad084bb4007498b15a3ac
Sasha <sasha7692@gmail.com>
2020-03-26 18:24:10 +0200

    Update net_check.sh

--- net_check.sh
+++ net_check.sh
@@ -6,69 +6,56 @@ set -x
 
 # Check operation system
 if echo $(hostnamectl | grep "Operating System: ") | grep -q "Ubuntu 18.04"; then
-    os="Ubuntu"
+   os="Ubuntu"
 elif echo $(hostnamectl | grep "Operating System: ") | grep -q "CentOS Linux 7"; then
-    os="Centos"
+   os="Centos"
+else
+   echo "Your operating system does not support this script."
+   exit 0
 fi
+
+connection_check_first_try(){
+   wget -q --spider google.com
+   # If connection true, first try
+   if [ $? -eq 0 ]; then
+      exit 0
+   # If connection false, first try
+   else
+      # Network restart
+	  if [ "$os" = "Ubuntu" ]; then
+         netplan apply
+	  elif [ "$os" = "Centos" ]; then
+	     systemctl restart network	  
+      fi
+      sleep 1
+   fi
+}
+
+connection_check_second_try(){
+   wget -q --spider google.com
+   # If connection true, second try
+   if [ $? -eq 0 ]; then
+      exit 0
+   # If connection false, second try
+   else
+      # Reboot
+      echo "No Connection"
+      reboot
+   fi
+}
 
 # If Ubuntu 18.04
-if echo $os -eq "Ubuntu"; then
-   # Check connection, first try
-   wget -q --spider google.com
-   # If connection true, first try
-   if [ $? -eq 0 ]; then
-      exit 0
-   # If connection false, first try
-   else
-      # Network restart
-      netplan apply
-      sleep 1
-      # Check connection, second try
-      wget -q --spider google.com
-      # If connection true, second try
-      if [ $? -eq 0 ]; then
-          exit 0
-      # If connection false, second try
-      else
-          # Reboot
-          echo "No Connection"
-          reboot
-      fi
-   fi
-fi
-
+if [ "$os" = "Ubuntu" ]; then
+   connection_check_first_try
+   connection_check_second_try
 # If Centos 7
-if echo $os -eq "Centos"; then
-   # Check wget, first try
+elif [ "$os" = "Centos" ]; then
+   # Check wget
    if echo $(yum list installed | grep wget) -eq ""; then
-     yum install -y wget > /dev/null;
+      yum install -y wget > /dev/null
    fi
-   # Check wget, second try
-   if echo $(yum list installed | grep wget) -eq ""; then
-     ;
-   fi
-   # Check connection, first try
-   wget -q --spider google.com
-   # If connection true, first try
-   if [ $? -eq 0 ]; then
-      exit 0
-   # If connection false, first try
-   else
-      # Network restart
-      systemctl restart network
-      sleep 1
-      # Check connection, second try
-      wget -q --spider google.com
-      # If connection true, second try
-      if [ $? -eq 0 ]; then
-          exit 0
-      # If connection false, second try
-      else
-          # Reboot
-          echo "No Connection"
-          reboot
-      fi
-   fi
+   connection_check_first_try
+   connection_check_second_try 
 fi
 
 # Off script logging
